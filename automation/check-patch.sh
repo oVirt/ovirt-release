1fb3f58aca04ddc8ec1cda234cc16bad618ff971
Sandro Bonazzola <sbonazzo@redhat.com>
2016-03-04 10:20:31 +0100

    automation: added standard ci
    Change-Id: I9b45b610ff957ac8a20ebad96579e39fefd1b4ca
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

---
+++ automation/check-patch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -xe
+[[ -d exported-artifacts ]] \
+|| mkdir -p exported-artifacts
+
+[[ -d tmp.repos ]] \
+|| mkdir -p tmp.repos
+
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+
+autoreconf -ivf
+./configure
+make distcheck
+rpmbuild \
+    -D "_topdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
+    -ta ovirt-release*.tar.gz
+
+mv *.tar.gz exported-artifacts
+find \
+    "$PWD/tmp.repos" \
+    -iname \*.rpm \
+    -exec mv {} exported-artifacts/ \;

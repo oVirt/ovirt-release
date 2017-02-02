7161aa806719638953bf5e330ee209e23d9e3684
Sandro Bonazzola <sbonazzo@redhat.com>
2017-02-02 11:12:24 +0100

    automation: fix repositories handling
    we had issues with yum repositories due to conflicts
    between epel and CentOS OpsTool
    
    Change-Id: Ie801c8f7aeec25358b149c65ff7f53411c986a25
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>
    (cherry picked from commit 8bbb61dfd220dec6076f0afdb3f4199a383096c6)

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -20,6 +20,13 @@ find \
     -iname \*.rpm \
     -exec mv {} exported-artifacts/ \;
 pushd exported-artifacts
+    #Restoring sane yum environment
+    yum reinstall -y system-release yum
+    [[ -d /etc/dnf ]] && dnf -y reinstall dnf-conf
+    [[ -d /etc/dnf ]] && sed -i -re 's#^(reposdir *= *).*$#\1/etc/yum.repos.d#' '/etc/dnf/dnf.conf'
+    yum install -y ovirt-release41-4*noarch.rpm
+    rm -f /etc/yum/yum.conf
+    yum repolist enabled
     yum --downloadonly install *noarch.rpm
 popd
 

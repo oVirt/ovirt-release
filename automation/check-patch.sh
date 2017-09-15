ddac3fcefbf11bc841077c779cf42493d7e70871
Sandro Bonazzola <sbonazzo@redhat.com>
2017-09-15 13:22:19 +0200

    centos: fix virt sig url
    oVirt 3.6 Virt SIG repo is available only till CentOS 7.3
    
    Change-Id: Iecea1ed38665d7e18adedc2ca07a4cd8f8a4de65
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -18,13 +18,3 @@ find \
     "$PWD/tmp.repos" \
     -iname \*.rpm \
     -exec mv {} exported-artifacts/ \;
-pushd exported-artifacts
-    #Restoring sane yum environment
-    yum reinstall -y system-release yum
-    [[ -d /etc/dnf ]] && dnf -y reinstall dnf-conf
-    [[ -d /etc/dnf ]] && sed -i -re 's#^(reposdir *= *).*$#\1/etc/yum.repos.d#' '/etc/dnf/dnf.conf'
-    yum install -y ovirt-release36-3*noarch.rpm
-    rm -f /etc/yum/yum.conf
-    yum repolist enabled
-    yum --downloadonly install *noarch.rpm
-popd

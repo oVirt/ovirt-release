436373570c384ac8bb5b66a99f796ce824218c22
Sandro Bonazzola <sbonazzo@redhat.com>
2017-09-19 15:33:05 +0200

    automation: check dnf binary existence
    for some reason, dnf configuration is available
    even in EL7 mock chroot but dnf binary is missing.
    
    Change-Id: Ic0685ca364ba1431132ed8c6c419b5d0b0d75002
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -24,7 +24,7 @@ pushd exported-artifacts
     #Restoring sane yum environment
     rm -f /etc/yum.conf
     yum reinstall -y system-release yum
-    [[ -d /etc/dnf ]] && dnf -y reinstall dnf-conf
+    [[ -d /etc/dnf ]] && [[ -x /usr/bin/dnf ]] && dnf -y reinstall dnf-conf
     [[ -d /etc/dnf ]] && sed -i -re 's#^(reposdir *= *).*$#\1/etc/yum.repos.d#' '/etc/dnf/dnf.conf'
     yum install -y ovirt-release-master-4*noarch.rpm
     rm -f /etc/yum/yum.conf

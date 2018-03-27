eb361845b9e746b74b1578e5b0d3aeb1894f6a99
Lev Veyde <lveyde@redhat.com>
2018-03-27 11:39:36 +0300

    build: ovirt-release42-4.2.2
    Change-Id: I64b36b7807e5b27782cd64ff3faee94d831672d6
    Signed-off-by: Lev Veyde <lveyde@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -5,8 +5,6 @@
 [[ -d tmp.repos ]] \
 || mkdir -p tmp.repos
 
-SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
-
 DISTVER="$(rpm --eval "%dist"|cut -c2-3)"
 PACKAGER=""
 if [[ "${DISTVER}" == "el" ]]; then
@@ -20,7 +18,6 @@ autoreconf -ivf
 make distcheck
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
-    -D "release_suffix ${SUFFIX}" \
     -ta ovirt-release*.tar.gz
 
 mv *.tar.gz exported-artifacts
@@ -34,7 +31,7 @@ pushd exported-artifacts
     ${PACKAGER} reinstall -y system-release ${PACKAGER}
     [[ -d /etc/dnf ]] && [[ -x /usr/bin/dnf ]] && dnf -y reinstall dnf-conf
     [[ -d /etc/dnf ]] && sed -i -re 's#^(reposdir *= *).*$#\1/etc/yum.repos.d#' '/etc/dnf/dnf.conf'
-    ${PACKAGER} install -y ovirt-release42-pre-4*noarch.rpm
+    ${PACKAGER} install -y ovirt-release42-4*noarch.rpm
     rm -f /etc/yum/yum.conf
     ${PACKAGER} repolist enabled
     DISTVER="$(rpm --eval "%dist"|cut -c2-)"

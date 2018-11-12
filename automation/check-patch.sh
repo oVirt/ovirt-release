524ce2cccd3cc299461e2d9cc497035be2f3f8d1
Sandro Bonazzola <sbonazzo@redhat.com>
2018-11-12 17:26:06 +0100

    build: post ovirt-4.3 branching
    Preparing for oVirt 4.3 beta release.
    At this time no repos exists for:
    - stable
    - snapshot
    - tested
    
    so skipping thos sub packages.
    
    Change-Id: If145a1b1d8da7ab06b13c8c4e224c84576aa63dc
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -22,7 +22,7 @@ pushd exported-artifacts
     [[ -d /etc/dnf ]] && [[ -x /usr/bin/dnf ]] && dnf -y reinstall dnf-conf
     [[ -d /etc/dnf ]] && sed -i -re 's#^(reposdir *= *).*$#\1/etc/yum.repos.d#' '/etc/dnf/dnf.conf'
     [[ -e /etc/dnf/dnf.conf ]] && echo "deltarpm=False" >> /etc/dnf/dnf.conf
-    ${PACKAGER} install -y ovirt-release-master-4*noarch.rpm
+    ${PACKAGER} install -y ovirt-release43-pre-4*noarch.rpm
     rm -f /etc/yum/yum.conf
     if [[ "${DISTVER}" == "el" ]]; then
         #Enable CR repo

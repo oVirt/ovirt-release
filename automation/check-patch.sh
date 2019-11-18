1ba5b84d660c21294e76d12f775afc2ab479524b
Sandro Bonazzola <sbonazzo@redhat.com>
2019-11-18 16:42:45 +0100

    build: post branching
    Change-Id: I437a89b8dd30cf889a115d218394548c48a8f826
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -31,7 +31,7 @@ pushd exported-artifacts
     [[ -d /etc/dnf ]] && [[ -x /usr/bin/dnf ]] && dnf -y reinstall dnf-conf
     [[ -d /etc/dnf ]] && sed -i -re 's#^(reposdir *= *).*$#\1/etc/yum.repos.d#' '/etc/dnf/dnf.conf'
     [[ -e /etc/dnf/dnf.conf ]] && echo "deltarpm=False" >> /etc/dnf/dnf.conf
-    ${PACKAGER} install -y ovirt-release-master-4*noarch.rpm
+    ${PACKAGER} install -y ovirt-release44-pre-4*noarch.rpm
     rm -f /etc/yum/yum.conf
     if [[ "${DISTVER}" == "el7" ]]; then
         #Enable CR repo

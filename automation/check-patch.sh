6dc06f500eafdede03eaf541108a869f3ef292f0
Sandro Bonazzola <sbonazzo@redhat.com>
2020-11-23 18:11:34 +0100

    fedora: drop fc30 config files
    fc30 gone EOL on 2020-05-26.
    See: https://fedoraproject.org/wiki/End_of_life
    
    Change-Id: Id45ad675903746c0e983c44eaf5682802d67e1a9
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -40,11 +40,6 @@ pushd exported-artifacts
         # without causing the test to fail.
         ${PACKAGER} --downloadonly install *noarch.rpm || true
     elif
-     [[ "$(rpm --eval "%dist")" == ".fc30" ]]; then
-        # fc30 support is broken, just provide a hint on what's missing
-        # without causing the test to fail.
-        ${PACKAGER} --downloadonly install *noarch.rpm || true
-    elif
      [[ "$(rpm --eval "%dist")" == ".el8" ]]; then
         # el8 engine support is broken, just provide a hint on what's missing
         # without causing the test to fail.
